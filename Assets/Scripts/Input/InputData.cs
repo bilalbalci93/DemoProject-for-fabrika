f3d0a6fa18ca2adf8fafd2956995737463a6e9eb
Bilal BALCI <bilal.balci@ozu.edu.tr>
2021-03-03 22:58:50 +0300

    Added Environment and Player

---
+++ Assets/Scripts/Input/InputData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fabrikademo.PlayerInput
+{
+    [CreateAssetMenu(menuName = "fabrikademo/Input/Input Data")]
+
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using fabrikademo.PlayerInput;
+
+namespace fabrikademo.PlayerMovement
+{
+
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private InputData _inputData;
+
+        private void Update()
+        {
+            _rigidbody.MovePosition(_rigidbody.transform.forward * _inputData.Vertical);
+            _rigidbody.MovePosition(_rigidbody.transform.right * _inputData.Horizontal);
+        }
+    }
+}
